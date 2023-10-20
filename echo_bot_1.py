ea9bce7650acaafd4f4babfede3beceae51bf71a
muhycspb <dimka.k@bk.ru>
2023-10-20 22:39:46 +0300

    init commit

--- echo_bot_1.py
+++ echo_bot_1.py
@@ -20,6 +20,21 @@ async def send_photo_echo(message: Message):
 async def send_sticker_echo(message: Message):
     await message.reply_sticker(message.sticker.file_id)
 
+async def send_audio_echo(message: Message):
+    await message.reply_sticker(message.audio.file_id)
+
+async def send_video_echo(message: Message):
+    await message.reply_sticker(message.video.file_id)
+
+async def send_voice_echo(message: Message):
+    await message.reply_sticker(message.voice.file_id)
+
+async def send_animation_echo(message: Message):
+    await message.reply_sticker(message.animation.file_id)
+
+async def send_video_note_echo(message: Message):
+    await message.reply_sticker(message.video_note.file_id)
+
 async def send_echo(message: Message):
     await message.reply(text=message.text)
 
@@ -27,6 +42,11 @@ dp.message.register(process_start_command, Command(commands='start'))
 dp.message.register(process_help_command, Command(commands='help'))
 dp.message.register(send_photo_echo, F.photo)
 dp.message.register(send_sticker_echo, F.sticker)
+dp.message.register(send_audio_echo, F.audio)
+dp.message.register(send_video_echo, F.video)
+dp.message.register(send_voice_echo, F.voice)
+dp.message.register(send_animation_echo, F.animation)
+dp.message.register(send_video_note_echo, F.video_note)
 dp.message.register(send_echo)
 
 if __name__ == '__main__':
